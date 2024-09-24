33ed507f37a1b67be153a7ec4c1a0621d6b92739
Vimal Sharma <88386120+vimalgits@users.noreply.github.com>
2024-09-24 16:19:06 +0530

    some changes

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class Footer extends Component {
+  render() {
+    return (
+      <div className='background text-light text-center p-2 pt-4 rounded-3'>
+        <p>copyright@Khabardaar.com</p>
+      </div>
+    );
+  }
+}
